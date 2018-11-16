0f5844cccb640be02036b69b63565ca8c186c88f
boazzang <chlqh4106@cu.ac.kr>
2018-11-16 14:11:50 +0900

    add replace.py

---
+++ python-programming/replace.py
@@ -0,0 +1,14 @@
+import sys
+
+src = sys.argv[1]
+dst = sys.argv[2]
+
+f = open(src, 'r')
+tab = f.read()
+f.close()
+
+space = tab.replace("\t", " "*4)
+
+f = open(dst, 'w')
+f.write(space)
+f.close
